ca4f250d8d396c338111703f4b72c99f2b3cb3ad
samthatcode <samthatcode@gmail.com>
2021-10-05 22:26:48 +0100

    Updated the js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ const showMenu = (toggleId, navId) => {
         })
     }
 }
-showMenu('nav-toggle', 'nav-menu')
+showMenu('nav-toggle', 'nav-menu');
 
 /*===== ACTIVE AND REMOVE MENU =====*/
 const navLink = document.querySelectorAll('.nav__link');
@@ -57,5 +57,104 @@ sr.reveal('.work__img', { interval: 200 });
 sr.reveal('.contact__input', { interval: 200 });
 
 
+/* First Form Validation Using Jquery */
+$(document).ready(function () {
 
+    $('#contact__form').submit(function (e) {
+        e.preventDefault();
+        var name = $('#name').val();
+        var email = $('#email').val();
 
+        var text = $('#text').val();
+
+        $(".error").remove();
+
+        if (name.length < 1) {
+            $('#name').after('<span class="error">This field is required</span>');
+        }
+        if (email.length < 1) {
+            $('#email').after('<span class="error">This field is required</span>');
+        }
+        if (text.length < 1) {
+            $('#text').after('<span class="error">This field is required</span>');
+        } else {
+            var regEx = /^[A-Z0-9][A-Z0-9._%+-]{0,63}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/;
+            var validEmail = regEx.test(email);
+            if (!validEmail) {
+                $('#email').after('<span class="error">Enter a valid email</span>');
+            }
+        }
+        if (password.length < 8) {
+            $('#password').after('<span class="error">Password must be at least 8 characters long</span>');
+        }
+    });
+
+    /* Form Validation Using Jquery Pluggin */
+
+    $('form[id="contact__form"]').validate({
+        rules: {
+            name: 'required',
+            text: 'required',
+            email: {
+                required: true,
+                email: true,
+            },
+            psword: {
+                required: true,
+                minlength: 8,
+            }
+        },
+        messages: {
+            name: 'This field is required',
+            text: 'This field is required',
+            email: 'Enter a valid email',
+            psword: {
+                minlength: 'Password must be at least 8 characters long'
+            }
+        },
+        submitHandler: function submit(form) {
+            form.submit();
+        }
+    });
+
+});
+
+/* Scroll to Top*/
+//Get the button:
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+/* Footer date */
+
+window.onload = function () {
+    //gets the year and put it in a variable
+    let currentYear = new Date().getFullYear();
+
+    //get the element we want to add by id
+    let d = document.getElementById("currentYear");
+
+    //convert the variable into a node
+    let e = document.createTextNode(currentYear);
+    
+    //add the node to the empty span tag
+    d.appendChild(e);
+
+};
